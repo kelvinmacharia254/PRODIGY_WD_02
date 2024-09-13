6eb2d3309fb655f1ddd5c8bfe23227fb7c91d1e4
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-09-13 14:16:16 +0300

    refactor: put start/stop and reset logic into functions callable by event handlers

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,20 @@
 // GUI elements
 const startButton= document.querySelector("#start")
 const lapButton= document.querySelector("#lap")
-const reset= document.querySelector("#reset")
+const resetButton= document.querySelector("#reset")
 
 const displayCount= document.querySelector("#display")
 
-function formatTime(time) {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time%3600) / 60);
-    const seconds = Math.floor(time % 60);
 
-    const formattedHours = String(hours).padStart(2, '0');
-   const formattedMinutes = String(minutes).padStart(2, '0');
-   const formattedSeconds = String(seconds).padStart(2, '0');
-
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-}
-
+// global vars
 let count = 0
 
 let timerNotRunning = true
 
 let interval ;
 
-const counter = () => {
-    interval = setInterval(() => {
-        count = count + 1
-        displayCount.textContent = `${formatTime(count)}`
-    }, 1000)
-}
 
-startButton.addEventListener("click",()=> {
+function stopWatch(){
     if(timerNotRunning){
         counter()
         startButton.textContent = "Stop"
@@ -40,11 +24,34 @@ startButton.addEventListener("click",()=> {
         timerNotRunning = true
         startButton.textContent = "Start"
     }
-})
+}
 
-reset.addEventListener("click",()=>{
+function resetStopWatch(){
     clearInterval(interval)
     timerNotRunning = true
     startButton.textContent = "Start"
     displayCount.textContent = formatTime(0)
-})
+}
+
+const counter = () => {
+    interval = setInterval(() => {
+        count = count + 1
+        displayCount.textContent = `${formatTime(count)}`
+    }, 1000)
+}
+
+function formatTime(time) {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time%3600) / 60);
+    const seconds = Math.floor(time % 60);
+
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+startButton.addEventListener("click",stopWatch)
+
+resetButton.addEventListener("click",resetStopWatch)
